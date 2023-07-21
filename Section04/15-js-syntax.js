06338814f501194645f7ca6b1abc4dced90f8e9b
nikojona <51277544+nikojona@users.noreply.github.com>
2023-07-21 11:44:25 +0700

    Create 15-js-syntax.js

---
+++ Section04/15-js-syntax.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        
+        <p id="demo"></p>
+        
+        <script>
+            let x, y , z;
+            
+            x = 5;
+            y = 6;
+            z = 7;
+
+            document.getElementById("demo").innerHTML = (x + y) - z;
+        </script>
+    </body>
+</html>
