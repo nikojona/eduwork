32b429710975c5b7792528e4983ef1d4a921ac2c
nikojona <51277544+nikojona@users.noreply.github.com>
2023-07-21 11:54:32 +0700

    Create 17-js-data-types.js

---
+++ Section04/17-js-data-types.js
@@ -0,0 +1,7 @@
+let length = 16; // <-- type data "Number"
+let lastName = "Johnson"; // <-- type data "String"
+
+const x = {
+  firstName: "John",
+  lastName: "Doe"
+}; // <-- type data "Object" (mutable object)
